e8c6d96445d4f0c549d71d76143536f9619d6f61
4eonardo <4eonardo@gmail.com>
2025-04-28 21:40:29 -0500

    Update hook.js

--- js/hook.js
+++ js/hook.js
@@ -1,1 +1,9 @@
 //file
+/*
+Claw.onKeyPressQuestion = function(event){
+var Q = event.target.value.toString().trim();
+if (event.key === 'Enter' && Q !== ''){
+event.target.value = '';
+Claw.ask(Q);
+}
+}
