dafdbc47579dbecaa5e8db645a7d5299926faaf4
4eonardo <4eonardo@gmail.com>
2026-02-19 21:20:04 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,16 @@
 //placeholder
+
+//lat.html
+//initialize get input
+var turncounter = 0;
+var contra = "";
+const source= document.getElementById("input");
+source.addEventListener("keypress", (e) => {if (e.key === "Enter"){repost(); turncounter++;}});
+function repost(){
+console.log(source.value);
+  source.reset();
+  contra = "the gods omniscient";
+  return contra;
+}
+
+alert(contra);
